3375a67687ee0ce26b9f5e5dc1bf778ed97125d6
Almir <almir.semic@yahoo.com>
2022-09-23 23:12:01 +0200

    implement todo list

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    author = models.ForeignKey('auth.User', related_name='todo', on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    state = models.CharField(max_length=100, blank=True, default='')
+    name = models.CharField(max_length=100, blank=True, default='')
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['created_date']
+        
+        
+    def save(self, *args, **kwargs):
+      super().save(*args, **kwargs)
--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from api.models import Todo
+
+
+
+
+class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Todo
+        fields = ['url', 'id', 'author', 'state', 'name', 'description', 'created_date']
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
- 
+    todo = serializers.HyperlinkedRelatedField(many=True, view_name='todo-detail', read_only=True)
+
     class Meta:
         model = User
-        fields = ['url', 'id', 'username']
+        fields = ['url', 'id', 'username', 'todo']

2f199dd82044d77f1c0b98de5b13a67f2c4907af
Almir <almir.semic@yahoo.com>
2022-10-03 04:58:35 +0200

    change password

--- todo_backend/urls.py
+++ todo_backend/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from api.views import ChangePasswordView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
 )
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/change-password/', ChangePasswordView.as_view(), name='change-password'),
 ]
 
 urlpatterns += [
